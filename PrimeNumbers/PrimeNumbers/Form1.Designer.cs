39e4f011786972f42b2bca9107342430bde37772
ilocereal <mythologyali@gmail.com>
2017-09-30 23:12:52 -0700

    Add files via upload

---
+++ PrimeNumbers/PrimeNumbers/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace PrimeNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TextOutput = new System.Windows.Forms.TextBox();
+            this.TextInput = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.calculationPanel = new System.Windows.Forms.Panel();
+            this.graphPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.graphButton = new System.Windows.Forms.Button();
+            this.calculationPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TextOutput
+            // 
+            this.TextOutput.Location = new System.Drawing.Point(12, 12);
+            this.TextOutput.Multiline = true;
+            this.TextOutput.Name = "TextOutput";
+            this.TextOutput.ReadOnly = true;
+            this.TextOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextOutput.Size = new System.Drawing.Size(608, 415);
+            this.TextOutput.TabIndex = 0;
+            // 
+            // TextInput
+            // 
+            this.TextInput.Location = new System.Drawing.Point(12, 434);
+            this.TextInput.Name = "TextInput";
+            this.TextInput.Size = new System.Drawing.Size(608, 20);
+            this.TextInput.TabIndex = 1;
+            this.TextInput.TextChanged += new System.EventHandler(this.TextInput_TextChanged);
+            this.TextInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextInputEnter);
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(527, 460);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(93, 51);
+            this.SendButton.TabIndex = 2;
+            this.SendButton.Text = "Prime Me Up Daddy";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 500);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Made by: Ali Bingol";
+            // 
+            // calculationPanel
+            // 
+            this.calculationPanel.Controls.Add(this.chart1);
+            this.calculationPanel.Controls.Add(this.graphPanel);
+            this.calculationPanel.Controls.Add(this.TextOutput);
+            this.calculationPanel.Controls.Add(this.SendButton);
+            this.calculationPanel.Controls.Add(this.graphButton);
+            this.calculationPanel.Controls.Add(this.TextInput);
+            this.calculationPanel.Location = new System.Drawing.Point(0, 0);
+            this.calculationPanel.Name = "calculationPanel";
+            this.calculationPanel.Size = new System.Drawing.Size(627, 530);
+            this.calculationPanel.TabIndex = 4;
+            // 
+            // graphPanel
+            // 
+            this.graphPanel.Location = new System.Drawing.Point(111, 12);
+            this.graphPanel.Name = "graphPanel";
+            this.graphPanel.Size = new System.Drawing.Size(608, 442);
+            this.graphPanel.TabIndex = 3;
+            this.graphPanel.Visible = false;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(608, 443);
+            this.chart1.TabIndex = 5;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // graphButton
+            // 
+            this.graphButton.AllowDrop = true;
+            this.graphButton.Location = new System.Drawing.Point(428, 461);
+            this.graphButton.Name = "graphButton";
+            this.graphButton.Size = new System.Drawing.Size(93, 50);
+            this.graphButton.TabIndex = 0;
+            this.graphButton.Text = "button1";
+            this.graphButton.UseVisualStyleBackColor = true;
+            this.graphButton.Click += new System.EventHandler(this.graphButtonClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 522);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calculationPanel);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Prime Number Finder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.calculationPanel.ResumeLayout(false);
+            this.calculationPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextOutput;
+        private System.Windows.Forms.TextBox TextInput;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel calculationPanel;
+        private System.Windows.Forms.Button graphButton;
+        private System.Windows.Forms.FlowLayoutPanel graphPanel;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ PrimeNumbers/PrimeNumbers/Form1.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/*
+ *Name: Ali
+ *Description: C# program that lets you calculate prime numbers. Two separate algorithms are used,
+ *             one for printing all divisors and the other for finding primes as quickly as possible.
+*/
+namespace PrimeNumbers
+{
+    public partial class Form1 : Form
+    {
+        int firstNumber;
+        int secondNumber;
+        public Thread T1;
+        public Thread T2;
+        public Thread T3;
+        public Thread T4;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            TextOutput.Text = ("Please enter your first number.\n");
+
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            enteringNumbers();
+        }
+
+
+        public bool resultFound;
+        public bool divisorFound;
+        public bool totalPrimesFound;
+        public bool firstNumberEntered;
+        public bool secondNumberEntered;
+
+        public List<string> primesList = new List<string>();        
+        public List<int> finalPrimes = new List<int>();   
+        public List<string> primesThread1 = new List<string>();
+        public List<string> primesThread2 = new List<string>();
+        public List<string> primesThread3 = new List<string>();
+        public List<string> primesThread4 = new List<string>();
+
+        public const int totalThreads = 4;
+
+        public int[] firstThreadLoad;
+        public int[] secondThreadLoad;
+        public int[] thirdThreadLoad;
+        public int[] fourthThreadLoad;
+
+
+        public void findPrimes1(int[] firstThreadLoad)
+        {
+            int Number = 1;
+            int iterate = 1;
+            for (Number = firstThreadLoad[0]; Number < firstThreadLoad[1]; Number++)
+            {
+                bool resultFound = false;
+                bool divisorFound = false;
+                int fastNumber = Convert.ToInt32(Math.Ceiling(Number / 2.0));
+                int lastDigit = Number % 10;
+                for (iterate = 1; iterate < fastNumber + 1; iterate++)
+                {
+                    while ((iterate <= fastNumber && resultFound is false) || fastNumber == 1)
+                    {
+
+                        if ((divisorFound is true && iterate >= fastNumber) || lastDigit % 2 == 0 || lastDigit % 5 == 0 || lastDigit % 10 == 0)
+                        {
+                            resultFound = true;
+                            //string primeArray = string.Join(",", primesList.ToArray());
+                            //TextOutput.AppendText(Number + " is not prime\n");
+                            //primesList.Clear();
+                            break;
+                        }
+                        else if (Number % iterate == 0 && iterate != Number && iterate != 1)
+                        {
+                            divisorFound = true;
+                            primesList.Add(Convert.ToString(iterate));
+                            iterate += 1;
+                        }
+
+                        else if (divisorFound is false && iterate >= fastNumber || Number == 2)
+                        {
+                            resultFound = true;
+                            totalPrimesFound = true;
+                            primesThread1.Add(Number + " is prime.");
+                            break;
+                        }
+                        else
+                        {
+                            iterate += 1;
+                        }
+                    }
+                }
+            }
+        }
+        
+        public void findPrimes2(int[] secondThreadLoad)
+        {
+            int Number = 1;
+            int iterate = 1;
+            for (Number = secondThreadLoad[0]; Number < secondThreadLoad[1]; Number++)
+            {
+                bool resultFound = false;
+                bool divisorFound = false;
+                int fastNumber = Convert.ToInt32(Math.Ceiling(Number / 2.0));
+                int lastDigit = Number % 10;
+                for (iterate = 1; iterate < fastNumber + 1; iterate++)
+                {
+                    while ((iterate <= fastNumber && resultFound is false) || fastNumber == 1)
+                    {
+
+                        if ((divisorFound is true && iterate >= fastNumber) || lastDigit % 2 == 0 || lastDigit % 5 == 0 || lastDigit % 10 == 0)
+                        {
+                            resultFound = true;
+                            //string primeArray = string.Join(",", primesList.ToArray());
+                            //TextOutput.AppendText(Number + " is not prime\n");
+                            //primesList.Clear();
+                            break;
+                        }
+                        else if (Number % iterate == 0 && iterate != Number && iterate != 1)
+                        {
+                            divisorFound = true;
+                            primesList.Add(Convert.ToString(iterate));
+                            iterate += 1;
+                        }
+
+                        else if (divisorFound is false && iterate >= fastNumber || Number == 2)
+                        {
+                            resultFound = true;
+                            totalPrimesFound = true;
+                            primesThread2.Add(Number + " is prime.");
+                            break;
+                        }
+                        else
+                        {
+                            iterate += 1;
+                        }
+                    }
+                }
+            }
+        }
+        public void findPrimes3(int[] thirdThreadLoad)
+        {
+            int Number = 1;
+            int iterate = 1;
+            for (Number = thirdThreadLoad[0]; Number < thirdThreadLoad[1]; Number++)
+            {
+                bool resultFound = false;
+                bool divisorFound = false;
+                int fastNumber = Convert.ToInt32(Math.Ceiling(Number / 2.0));
+                int lastDigit = Number % 10;
+                for (iterate = 1; iterate < fastNumber + 1; iterate++)
+                {
+                    while ((iterate <= fastNumber && resultFound is false) || fastNumber == 1)
+                    {
+
+                        if ((divisorFound is true && iterate >= fastNumber) || lastDigit % 2 == 0 || lastDigit % 5 == 0 || lastDigit % 10 == 0)
+                        {
+                            resultFound = true;
+                            //string primeArray = string.Join(",", primesList.ToArray());
+                            //TextOutput.AppendText(Number + " is not prime\n");
+                            //primesList.Clear();
+                            break;
+                        }
+                        else if (Number % iterate == 0 && iterate != Number && iterate != 1)
+                        {
+                            divisorFound = true;
+                            primesList.Add(Convert.ToString(iterate));
+                            iterate += 1;
+                        }
+
+                        else if (divisorFound is false && iterate >= fastNumber || Number == 2)
+                        {
+                            resultFound = true;
+                            totalPrimesFound = true;
+                            primesThread3.Add(Number + " is prime.");
+                            break;
+                        }
+                        else
+                        {
+                            iterate += 1;
+                        }
+                    }
+                }
+            }
+        }
+        public void findPrimes4(int[] fourthThreadLoad)
+        {
+            int Number = 1;
+            int iterate = 1;
+            for (Number = fourthThreadLoad[0]; Number < fourthThreadLoad[1]; Number++)
+            {
+                bool resultFound = false;
+                bool divisorFound = false;
+                int fastNumber = Convert.ToInt32(Math.Ceiling(Number / 2.0));
+                int lastDigit = Number % 10;
+                for (iterate = 1; iterate < fastNumber + 1; iterate++)
+                {
+                    while ((iterate <= fastNumber && resultFound is false) || fastNumber == 1)
+                    {
+
+                        if ((divisorFound is true && iterate >= fastNumber) || lastDigit % 2 == 0 || lastDigit % 5 == 0 || lastDigit % 10 == 0)
+                        {
+                            resultFound = true;
+                            //string primeArray = string.Join(",", primesList.ToArray());
+                            //TextOutput.AppendText(Number + " is not prime\n");
+                            //primesList.Clear();
+                            break;
+                        }
+                        else if (Number % iterate == 0 && iterate != Number && iterate != 1)
+                        {
+                            divisorFound = true;
+                            primesList.Add(Convert.ToString(iterate));
+                            iterate += 1;
+                        }
+
+                        else if (divisorFound is false && iterate >= fastNumber || Number == 2)
+                        {
+                            resultFound = true;
+                            totalPrimesFound = true;
+                            primesThread4.Add(Number + " is prime.");
+                            break;
+                        }
+                        else
+                        {
+                            iterate += 1;
+                        }
+                    }
+                }
+            }
+        }
+
+        public void findPrimesandDivisors(int startNumber, int endNumber)
+        
+        {
+            int Number = 1;
+            for (Number = startNumber; Number < endNumber; Number++)
+            {
+                resultFound = false;
+                divisorFound = false;
+                int iterate = 1;
+                while (iterate <= Number && resultFound is false)
+                {
+                    for (iterate = 1; iterate < Number + 1; iterate++)
+                    {
+                        if (Number % iterate == 0 && iterate != Number - 1 && iterate != 1)
+                        {
+                            divisorFound = true;
+                            primesList.Add(Convert.ToString(iterate));
+
+                        }
+                        else if (divisorFound is true && iterate == Number - 1)
+                        {
+                            resultFound = true;
+                            string primeArray = string.Join(",", primesList.ToArray());
+                            TextOutput.AppendText(Number + " is not prime, it is divisible by " + primeArray + "\n");
+                            primesList.Clear();
+                            break;
+                        }
+                        else if (divisorFound is false && iterate == Number - 1)
+                        {
+                            resultFound = true;
+                            totalPrimesFound = true;
+                            finalPrimes.Add(Number);
+                            TextOutput.AppendText(Number + " is prime.\n");
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                }
+            }
+        }
+        
+        public void enteringNumbers()
+        {
+            if (firstNumberEntered is false)
+            {
+
+                if (TextInput.Text == "")
+                {
+                    TextOutput.AppendText("Please enter a number number.\n");
+                }
+                else
+                {
+                    firstNumber = Convert.ToInt32(TextInput.Text);
+                    TextOutput.AppendText("Please enter your second number.\n");
+                    firstNumberEntered = true;
+                    TextInput.Clear();
+
+                }
+            }
+            else if (firstNumberEntered is true && secondNumberEntered is false)
+            {
+                
+                if (TextInput.Text == "")
+                {
+                    TextOutput.AppendText("Please enter a number number.\n");
+                }
+                else
+                {
+                    
+                    secondNumber = (Convert.ToInt32(TextInput.Text) + 1);
+                    secondNumberEntered = true;
+                    threadDivision();
+                    Thread T1 = new Thread(o => findPrimes1(firstThreadLoad));
+                    Thread T2 = new Thread(o => findPrimes2(secondThreadLoad));
+                    Thread T3 = new Thread(o => findPrimes3(thirdThreadLoad));
+                    Thread T4 = new Thread(o => findPrimes4(fourthThreadLoad));
+                    long tick1 = Environment.TickCount;
+                    TextInput.Clear();
+                    T1.Start();
+                    T2.Start();
+                    T3.Start();
+                    T4.Start();
+                    List<string> totalPrimesList = new List<string>();
+                    while (T1.IsAlive || T2.IsAlive || T3.IsAlive || T4.IsAlive);
+                    long tick2 = Environment.TickCount;
+                    long duration = tick2 - tick1;
+                    totalPrimesList = primesThread1.Concat(primesThread2).Concat(primesThread3).Concat(primesThread4).ToList();
+                    string str_totalPrimesList = string.Join(System.Environment.NewLine, totalPrimesList);
+
+                    TextOutput.AppendText(str_totalPrimesList + System.Environment.NewLine + System.Environment.NewLine);                   
+                    TextOutput.AppendText("The calculation took: " + duration.ToString() +" miliseconds."+ System.Environment.NewLine);
+                    TextOutput.AppendText("There were " + totalPrimesList.Count() + " primes found.");
+                    
+
+
+                }
+            }
+        }
+        public  void threadDivision()
+        {
+            int perThreadLoad = (secondNumber - firstNumber) / totalThreads;
+            firstThreadLoad =  new int[] { firstNumber,  perThreadLoad};
+            secondThreadLoad = new int[] { perThreadLoad+1, 2 *(perThreadLoad) };
+            thirdThreadLoad = new int[] { (2 * perThreadLoad) + 1, 3 * perThreadLoad };
+
+            fourthThreadLoad = new int[] { (3 * perThreadLoad) + 1, 4 * perThreadLoad };
+        }
+        private void TextInputEnter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                SendButton.PerformClick();
+            }
+        }
+
+
+            private void currentThread_TextChanged(object sender, EventArgs e)
+            {
+
+            }
+
+        private void TextInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void graphButtonClicked(object sender, EventArgs e)
+        {
+            //calculationPanel.Controls.Add(TextInput);
+           // calculationPanel.Controls.Add(TextOutput);
+           // calculationPanel.Controls.Add(SendButton);
+
+            calculationPanel.Visible = false;
+            graphPanel.Visible = true;
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+  
+
+}
+        
+    
